bbb1bedfcb4f219ceb8713e4ea86e8cb58bd77e5
Amit Kumar <akchoudhary966@outlook.com>
2018-10-04 14:26:18 +0530

    React Based Admin Dashboard

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './Table';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            tableData: [
+                {
+                    fname: "Amit",
+                    lname: "Kumar"
+                },
+                {
+                    fname: "Sumit",
+                    lname: "Kumar"
+                },
+                {
+                    fname: "Akash",
+                    lname: "Kumar"
+                },
+            ],
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello ! React</h1>
+                <div className="continer">
+                <Table tableData={this.state.tableData} />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
